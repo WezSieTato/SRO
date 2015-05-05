2e8954174ea674159d0364cdc4a1f4497d4b4687
WezSieTato <le0n.uo@gmail.com>
2015-05-05 20:22:02 +0200

    TaskManager class

--- Core/src/main/java/rso/core/taskmanager/Task.java
+++ Core/src/main/java/rso/core/taskmanager/Task.java
@@ -2,12 +2,15 @@ package rso.core.taskmanager;
 
 import com.google.protobuf.GeneratedMessage;
 
+import java.lang.ref.WeakReference;
+
 /**
  * Created by modz on 2015-04-29.
  */
 public abstract class Task {
 
     private int priority = 4;
+    private WeakReference< TaskManager> taskManager = null;
 
     /**
      * Przetwarza wiadomosc
@@ -24,11 +27,23 @@ public abstract class Task {
      */
     abstract public boolean canProcessMessage(TaskMessage taskMessage);
 
+    public void removeFromManager(){
+        getTaskManager().addTaskToDeleteList(this);
+    }
+
     public int getPriority() {
         return priority;
     }
 
     public void setPriority(int priority) {
         this.priority = priority;
+    }
+
+    public void setTaskManager(TaskManager taskManager){
+        this.taskManager = new WeakReference<TaskManager>(taskManager);
+    }
+
+    public TaskManager getTaskManager(){
+        return taskManager.get();
     }
 }
--- Core/src/main/java/rso/core/taskmanager/TaskManager.java
+++ Core/src/main/java/rso/core/taskmanager/TaskManager.java
@@ -2,6 +2,8 @@ package rso.core.taskmanager;
 
 import rso.core.events.RSOEvent;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -12,10 +14,14 @@ public class TaskManager implements  Runnable{
 
     private LinkedBlockingQueue<TaskMessage> messageQueue = new LinkedBlockingQueue<TaskMessage>();
     private LinkedList<Task> taskList = new LinkedList<Task>();
+    private LinkedList<Task> taskToDeleteList = new LinkedList<Task>();
 
     public void run() {
         while(true){
             TaskMessage taskMessage = messageQueue.poll();
+
+            removeTasksToDelete();
+
             for (Task task : taskList) {
                 if(task.canProcessMessage(taskMessage))
                     if(!task.processMessage(taskMessage))
@@ -25,7 +31,26 @@ public class TaskManager implements  Runnable{
     }
 
     public void addTask(Task task){
+        task.setTaskManager(this);
         taskList.add(task);
+
+        Collections.sort(taskList, new Comparator<Task>() {
+            public int compare(Task o1, Task o2) {
+                return o1.getPriority() - o2.getPriority();
+            }
+        });
+    }
+
+    public void addTaskToDeleteList(Task task){
+        taskToDeleteList.add(task);
+    }
+
+    private void removeTasksToDelete(){
+        for(Task task : taskToDeleteList){
+            taskList.remove(task);
+        }
+
+        taskToDeleteList.clear();
     }
 
     public void removeTask(Task task){
