6299fba8b89b6e61ab3f7c88bc57622a2272b250
Jarosław Kornata <jkornata@hotmail.com>
2015-05-28 18:29:00 +0200

    Update bazy

--- etc/baza.sql
+++ etc/baza.sql
@@ -1,25 +1,45 @@
+DROP DATABASE IF EXISTS `RSO`;
 CREATE DATABASE `RSO` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 use `RSO`;
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `student` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
   `birth_date` datetime DEFAULT NULL,
   `uuid` varchar(36) DEFAULT NULL,
+  `timestamp` DATETIME DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3502 DEFAULT CHARSET=utf8;
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8;
 
+DROP TABLE IF EXISTS `class`;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `class` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `uuid` varchar(36) DEFAULT NULL,
+  `timestamp` DATETIME DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8;
+
+DROP TABLE IF EXISTS `student_class`;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `student_class` (
   `id_student` int(11) NOT NULL,
   `id_class` int(11) NOT NULL,
   `timestamp` datetime DEFAULT NULL,
+  `uuid` VARCHAR(36) DEFAULT NULL,
   PRIMARY KEY (`id_student`,`id_class`),
   KEY `fk_student_group_group_idx` (`id_class`),
   CONSTRAINT `fk_student_group_group` FOREIGN KEY (`id_class`) REFERENCES `class` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
